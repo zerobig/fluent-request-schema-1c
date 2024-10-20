6d409fe3eb1ec6eee7dfa097a717d990048c0a1b
Ilya Bushin <big@mail.ru>
2024-10-20 23:10:54 +0300

    docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -35,6 +35,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          sidebarCallapsible: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
